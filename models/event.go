e549c7204ea03836eb4a8d80351448ec7a5d67b5
dario-nihil <dario.nihil@gmail.com>
2024-05-29 20:00:50 +0200

    implemented updating event

--- models/event.go
+++ models/event.go
@@ -15,8 +15,6 @@ type Event struct {
 	UserID      int
 }
 
-var events = []Event{}
-
 func (e *Event) Save() error {
 	// safe query protected against sql-injection attack
 	query := `
@@ -73,4 +71,20 @@ func GetEventByID(id int64) (*Event, error) {
 	}
 
 	return &event, nil
+}
+
+func (e Event) Update() error {
+	query := `
+	UPDATE events
+	SET name = ?, description = ?, location = ?, dateTime = ?
+	WHERE id = ?`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.ID)
+	return err
 }
