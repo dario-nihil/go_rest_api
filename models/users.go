bcc790c9d4c85eecc9f6748ac5b004362371e873
dario-nihil <dario.nihil@gmail.com>
2024-05-31 09:56:47 +0200

    started implementing login

--- models/users.go
+++ models/users.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"errors"
+
 	"github.com/dario-nihil/go_rest_api/db"
 	"github.com/dario-nihil/go_rest_api/utils"
 )
@@ -37,4 +39,23 @@ func (u *User) Save() error {
 
 	u.ID = userId
 	return err
+}
+
+func (u User) ValidateCredentials() error {
+	query := `SELECT password FROM users WHERE email = ?`
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+	if err != nil {
+		return errors.New("Invalid credentials")
+	}
+
+	passwordIsValid := utils.CheckPasswordHash(u.Password, retrievedPassword)
+
+	if !passwordIsValid {
+		return errors.New("Invalid credentials")
+	}
+
+	return nil
 }
--- routes/users.go
+++ routes/users.go
@@ -27,3 +27,21 @@ func signup(context *gin.Context) {
 
 	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully"})
 }
+
+func login(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data"})
+		return
+	}
+
+	err = user.ValidateCredentials()
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Could not authentica user."})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Login successfull"})
+}
