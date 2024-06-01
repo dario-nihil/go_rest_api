938b67321d274165b4249e904bf0ed97fe9c8aa1
dario-nihil <dario.nihil@gmail.com>
2024-06-01 15:08:37 +0200

    implemented validation token logic

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,4 +17,30 @@ func GenerateToken(email string, userId int64) (string, error) {
 	})
 
 	return token.SignedString([]byte(SECRETKEY))
+}
+
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		_, ok := t.Method.(*jwt.SigningMethodHMAC)
+
+		if !ok {
+			return nil, errors.New("Unexpected signin method")
+		}
+
+		return SECRETKEY, nil
+	})
+
+	if !parsedToken.Valid {
+		return errors.New("Inavlid token")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	// if !ok {
+	// 	return errors.New("Imvalid token claims")
+	// }
+
+	// email, _ := claims["email"].(string)
+	// userId, _ := claims["userId"].(int64)
+
+	return nil
 }
