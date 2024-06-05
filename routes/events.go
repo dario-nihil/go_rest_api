6bb2705bc5eb9e5c096dc756f88264c3f339dc93
dario-nihil <dario.nihil@gmail.com>
2024-06-05 19:23:04 +0200

    implemented routes group and authorization

--- routes/events.go
+++ routes/events.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 
 	"github.com/dario-nihil/go_rest_api/models"
-	"github.com/dario-nihil/go_rest_api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -37,27 +36,16 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized."})
-		return
-	}
-
-	userId, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
-		return
-	}
-
 	var event models.Event
 
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request data."})
 		fmt.Println(err)
 		return
 	}
 
+	userId := context.GetInt64("userId")
 	event.UserID = userId
 
 	err = event.Save()
